3b2dc65a4bc730fba04c8f8664ac0b30e6885bef
visudo <visudo20179C@protonmail.com>
2021-09-07 19:25:05 -0400

    prepares the project for hosting

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,79 +1,93 @@
 <template>
   <div class="bg-black w-full h-screen ml-auto mr-auto pt-10 font-mono" style="color: #B12322;">
-	<h1 class="ml-auto mr-auto text-xl w-2/3 mb-5 border rounded-lg border-gray-500">Stats for: {{player}}</h1>
-	<div v-if="responseDataCompute != null" class="bg-black">
-		<h1 class="text-xl mb-8">Account Level: {{this.responseData.global.level}}</h1>
-		<div v-tooltip.top-center="nextLevelMessage" class="h-3 w-3/4 relative max-w-xl mr-auto ml-auto rounded-full overflow-hidden">
-    		<div class="w-full h-full bg-black border rounded-lg border-gray-500 absolute"></div>
-    		<div class="h-full bg-red-500 absolute" :style="barStyle"></div>
-		</div>
-		<h1 class="text-xl mt-6 mb-4 font-black">{{rankedSplit}}</h1>
-		<table class="ml-auto mr-auto table-fixed w-3/4 divide-y divide-gray-200 border border-gray-500">
-			<tr class="border rounded-lg border-gray-500">
-				<th>Rank</th>
-				<th>Total RP</th>
-				<th>Rank #</th>
-			</tr>
-			<tr v-if="responseData.global.rank.ladderPosPlatform == -1">
-				<td>{{this.responseData.global.rank.rankName}} {{this.responseData.global.rank.rankDiv}}
-					<img class="ml-auto mr-auto max-h-16 max-w-16" :src="this.responseData.global.rank.rankImg"/>
-				</td>
-				<td>{{this.responseData.global.rank.rankScore}}</td>
-				<td>N/A</td>
-			</tr>
-			<tr v-else>
-				<td>{{this.responseData.global.rank.rankName}}
-					<img class="ml-auto mr-auto max-h-16 max-w-16" :src="this.responseData.global.rank.rankImg"/>
-				</td>
-				<td>{{this.responseData.global.rank.rankScore}}</td>
-				<td># {{this.responseData.global.rank.ladderPosPlatform}}</td>
-			</tr>
-		</table>
-		<div class="mt-4 mb-4 max-h-1/2 bg-black">
-			<h1 class="text-xl">{{player}}'s currently selected legend: </h1>
-			<div class="ml-auto mr-auto mt-5 text-bold text-2xl w-64 h-10 bg-black border rounded-lg border-gray-500">
-				{{this.responseData.legends.selected.LegendName}}
+	<div v-if="!hasSearchParams">
+		<div class="mb-4">No name provided in search param</div>
+		<div class="mb-4">Please provide the name of your origin account in the "name" parameter like so:</div>
+		<div class="mb-4">https://apex-stats.visudo.me?name=YOUR_NAME_HERE</div> 
+		<div class="mb-4"> OR </div>
+		<div class="mb-4">https://apex-stats.visudo.me/?name=YOUR_NAME_HERE</div>
+	</div>
+	<div v-else-if="errorMessage != null">
+		<div class="mb-4">{{this.errorMessage}}</div>
+		<div>Check the name and spelling and try again.</div>
+	</div>
+	<div v-else>
+		<h1 class="ml-auto mr-auto text-xl w-2/3 mb-5 border rounded-lg border-gray-500">Stats for: {{player}}</h1>
+		<div v-if="responseDataCompute != null" class="bg-black">
+			<h1 class="text-xl mb-8">Account Level: {{this.responseData.global.level}}</h1>
+			<div v-tooltip.top-center="nextLevelMessage" class="h-3 w-3/4 relative max-w-xl mr-auto ml-auto rounded-full overflow-hidden">
+				<div class="w-full h-full bg-black border rounded-lg border-gray-500 absolute"></div>
+				<div class="h-full bg-red-500 absolute" :style="barStyle"></div>
 			</div>
-			<img class="object-contain ml-auto mr-auto h-48 w-full" :src="this.responseData.legends.selected.ImgAssets.icon"/>
-			<h1 class="text-xl">Equipped Trackers:</h1>
-			<ul>
-				<div v-for="item in this.responseData.legends.selected.data">
-					<li class="w-2/3 ml-auto mr-auto border border-gray-500">{{item.name}}: {{item.value}}</li>
+			<h1 class="text-xl mt-6 mb-4 font-black">{{rankedSplit}}</h1>
+			<table class="ml-auto mr-auto table-fixed w-3/4 divide-y divide-gray-200 border border-gray-500">
+				<tr class="border rounded-lg border-gray-500">
+					<th>Rank</th>
+					<th>Total RP</th>
+					<th>Rank #</th>
+				</tr>
+				<tr v-if="responseData.global.rank.ladderPosPlatform == -1">
+					<td>{{this.responseData.global.rank.rankName}} {{this.responseData.global.rank.rankDiv}}
+						<img class="ml-auto mr-auto max-h-16 max-w-16" :src="this.responseData.global.rank.rankImg"/>
+					</td>
+					<td>{{this.responseData.global.rank.rankScore}}</td>
+					<td>N/A</td>
+				</tr>
+				<tr v-else>
+					<td>{{this.responseData.global.rank.rankName}}
+						<img class="ml-auto mr-auto max-h-16 max-w-16" :src="this.responseData.global.rank.rankImg"/>
+					</td>
+					<td>{{this.responseData.global.rank.rankScore}}</td>
+					<td># {{this.responseData.global.rank.ladderPosPlatform}}</td>
+				</tr>
+			</table>
+			<div class="mt-4 mb-4 max-h-1/2 bg-black">
+				<h1 class="text-xl">{{player}}'s currently selected legend: </h1>
+				<div class="ml-auto mr-auto mt-5 text-bold text-2xl w-64 h-10 bg-black border rounded-lg border-gray-500">
+					{{this.responseData.legends.selected.LegendName}}
 				</div>
-			</ul>
-		</div>
-		<div class="pb-5 bg-black"> 
-			<p class="mb-2"> See {{player}}'s trackers for: </p>
-			<t-select
-				placeholder="Select a Legend"
-				v-model="selectedLegend"
-				:options="this.legendOptions"
-				@change="showLegendTrackers()"
-			/>
-		</div>
-		<div v-if="selectedLegend && selectedLegendData" class="bg-black pb-10">
-			<h1 class="text-xl">Trackers:</h1>
-			<div v-if="selectedLegendData.data !== undefined">
+				<img class="object-contain ml-auto mr-auto h-48 w-full" :src="this.responseData.legends.selected.ImgAssets.icon"/>
+				<h1 class="text-xl">Equipped Trackers:</h1>
 				<ul>
-					<div v-for="item in selectedLegendData.data">
+					<div v-for="item in this.responseData.legends.selected.data">
 						<li class="w-2/3 ml-auto mr-auto border border-gray-500">{{item.name}}: {{item.value}}</li>
 					</div>
 				</ul>
 			</div>
-			<div v-else>
-				None Equipped
+			<div class="pb-5 bg-black"> 
+				<p class="mb-2"> See {{player}}'s trackers for: </p>
+				<t-select
+					placeholder="Select a Legend"
+					v-model="selectedLegend"
+					:options="this.legendOptions"
+					@change="showLegendTrackers()"
+				/>
+			</div>
+			<div v-if="selectedLegend && selectedLegendData" class="bg-black pb-10">
+				<h1 class="text-xl">Trackers:</h1>
+				<div v-if="selectedLegendData.data !== undefined">
+					<ul>
+						<div v-for="item in selectedLegendData.data">
+							<li class="w-2/3 ml-auto mr-auto border border-gray-500">{{item.name}}: {{item.value}}</li>
+						</div>
+					</ul>
+				</div>
+				<div v-else>
+					None Equipped
+				</div>
 			</div>
 		</div>
+		<div v-else>
+			<div class="text-lg">Loading... Please Wait... </div>
+			<half-circle-spinner
+			  :animation-duration="1000"
+			  :size="100"
+			  color="#B12322"
+			  class="m-auto"
+			/>
+		</div>	
 	</div>
-	<div v-else>
-		<div class="text-lg">Loading... Please Wait... </div>
-		<half-circle-spinner
-		  :animation-duration="1000"
-		  :size="100"
-		  color="#B12322"
-		  class="m-auto"
-		/>
-	</div>	
+	<p class="ml-auto mr-auto mt-20 text-sm w-2/3"><i>This site was made with the help of the mozambiquehe.re API. Visit their site <a href="https://apexlegendsapi.com/" _target="blank" class="text-gray-200">here</a> to learn more about the project and consider donating/becoming a supporter to them on Patreon.</i></p>
   </div>
 </template>
 
@@ -86,12 +100,14 @@ export default {
 	components: {HalfCircleSpinner, VTooltip},
 	data() {
 		return {
-			player: "visudo20179C",
+			player: null,
 			responseData: null,
 			legendOptions: [],
 			selectedLegend: null,
 			selectedLegendData: null,
 			levelMessage: null,
+			errorMessage: null,
+			hasSearchParams: false, 
 		}
 	},
 	computed: {
@@ -115,8 +131,13 @@ export default {
 	},
 	methods: {
 		processData(data) {
-			this.responseData = data
-			this.setupLegendOptions()
+			if(data.Error) {
+				this.errorMessage = data.Error
+			}
+			else {
+				this.responseData = data
+				this.setupLegendOptions()
+			}
 		},
 		setupLegendOptions() {
 			for(var elem in this.responseData.legends.all) {
@@ -128,14 +149,19 @@ export default {
 		}
 	},
 	created() {
-		fetch(
-			"https://api.mozambiquehe.re/bridge?version=5&platform=PC&player="+this.player+"&auth=QYMCCkwuWGEeBQFL30tT",
-			{
-				method: 'GET',
-			}
-		)
-		.then(response => response.json())
-		.then(data => this.processData(data))
+		let params = new URLSearchParams(document.location.search.substring(1))
+		if(params.get("name") != null) {
+			this.player = params.get("name")
+			this.hasSearchParams = true
+			fetch(
+				"https://api.mozambiquehe.re/bridge?version=5&platform=PC&player="+this.player+"&auth=QYMCCkwuWGEeBQFL30tT",
+				{
+					method: 'GET',
+				}
+			)
+			.then(response => response.json())
+			.then(data => this.processData(data))
+		}
 	},
 }
 </script>
