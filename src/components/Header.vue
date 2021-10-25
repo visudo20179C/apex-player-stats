142bdc790450dbeff2c8a1dfa8b942bd0e220be1
visudo <visudo20179C@protonmail.com>
2021-10-24 23:21:59 -0400

    updates

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
 			<div class="text-lg font-bold mt-auto mb-auto ml-2">Apex Player Stats</div>
 		</div>
 		<div class="flex mt-auto mb-auto text-lg font-bold">
-			<a class="ml-2 mr-2 hover:text-red-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60" href="https://visudo20179c.github.io/cjk-cv/" target="_blank">Me</a> |
+			<a class="ml-2 mr-2 hover:text-red-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60" href="https://visudo.me" target="_blank">Me</a> |
 			<a class="ml-2 mr-4 hover:text-red-500 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-60" href="https://github.com/visudo20179C/apex-player-stats" target="_blank">GitHub</a>
 		</div>
 	</div>
